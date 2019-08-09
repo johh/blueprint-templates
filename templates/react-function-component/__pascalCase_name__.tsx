a534c9d892213d57c4264ccc3a642c6608b5844e
johh <code@johh.net>
2019-08-09 13:58:48 +0200

    FEATURE: react function component

---
+++ templates/react-function-component/__pascalCase_name__.tsx
@@ -0,0 +1,14 @@
+import React, {
+	FunctionComponent,
+} from 'react';
+
+
+interface {{pascalCase name}}Props {
+
+}
+
+const {{pascalCase name}}: FunctionComponent<{{pascalCase name}}Props> = ( props ) => {
+	return null;
+};
+
+export default {{pascalCase name}};
