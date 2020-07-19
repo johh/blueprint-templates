712f765cba2cca43ece32a535c05be5ee23417ec
johh <code@johh.net>
2020-07-19 22:53:24 +0200

    feat: add react-fc-no-props

---
+++ templates/react-fc-no-props/__pascalCase_name__.tsx
@@ -0,0 +1,14 @@
+import React, {
+	FunctionComponent,
+} from 'react';
+
+
+const {{pascalCase name}}: FunctionComponent = ({ children }) => {
+	return (
+		<>
+			{ children }
+		</>
+	);
+};
+
+export default {{pascalCase name}};
