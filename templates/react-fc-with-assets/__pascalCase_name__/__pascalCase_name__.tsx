119733ed935505fab8ba9338c6e0b62796564e78
johh <code@johh.net>
2020-07-19 23:01:42 +0200

    feat: add react-fc-with-assets

---
+++ templates/react-fc-with-assets/__pascalCase_name__/__pascalCase_name__.tsx
@@ -0,0 +1,20 @@
+import React, {
+	FunctionComponent,
+} from 'react';
+
+
+interface {{pascalCase name}}Props {
+
+}
+
+const {{pascalCase name}}: FunctionComponent<{{pascalCase name}}Props> = ({
+	children,
+}) => {
+	return (
+		<>
+			{ children }
+		</>
+	);
+};
+
+export default {{pascalCase name}};
